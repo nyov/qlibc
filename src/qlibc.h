12fa54f4db9a96da9fa3d2a25379bf7c077b6d1f
seungyoung.kim <wolkykim@gmail.com>
2011-03-04 21:02:56 +0000

    Added qStrCpyn()
    git-svn-id: https://svn.qdecoder.org/qlibc/trunk@33 9abf7928-25fc-df11-b5bf-0015c5f490bc

--- src/qlibc.h
+++ src/qlibc.h
@@ -581,6 +581,7 @@ extern	char*		qStrTrimTail(char *str);
 extern	char*		qStrUnchar(char *str, char head, char tail);
 extern	char*		qStrReplace(const char *mode, char *srcstr, const char *tokstr, const char *word);
 extern	char*		qStrCpy(char *dst, size_t size, const char *src);
+extern	char*		qStrCpyn(char *dst, size_t size, const char *src, size_t nbytes);
 extern	char*		qStrDupf(const char *format, ...);
 extern	char*		qStrGets(char *buf, size_t size, char **offset);
 extern	char*		qStrUpper(char *str);
